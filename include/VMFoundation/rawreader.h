88e59a9de78497693fe1fc920f74eff9d30743db
koishi <qwq@koishi.top>
2020-01-22 17:39:36 +0800

    gcc-7.5 compatible

--- include/VMFoundation/rawreader.h
+++ include/VMFoundation/rawreader.h
@@ -9,46 +9,6 @@
 
 namespace vm
 {
-class VMFOUNDATION_EXPORTS RawReader
-{
-	std::string fileName;
-	vm::Size3 dimensions;
-	size_t voxelSize;
-	uint64_t offset;
-	//std::shared_ptr<IFileMappingPluginInterface> io;
-	::vm::Ref<IFileMapping> io;
-	unsigned char *ptr;
-	uint64_t seekAmt;
-
-public:
-	using PosType = unsigned long long;
-	using OffsetType = unsigned long long;
-	using SizeType = std::size_t;
-
-	RawReader( const std::string &fileName,
-			   const vm::Size3 &dimensions, size_t voxelSize );
-	~RawReader();
-	// Read a region of volume data from the file into the buffer passed.
-	// It's assumed the buffer passed has enough room. Returns the
-	// number voxels read
-
-	size_t readRegion( const vm::Size3 &start,
-					   const vm::Size3 &size, unsigned char *buffer );
-
-private:
-	std::size_t readRegion__( const vm::Size3 &start, const vm::Size3 &size, unsigned char *buffer );
-	bool convexRead( const vm::Size3 &size )
-	{
-		/// A minimum continuous unit for reading
-
-		// 3 cases for convex reads:
-		// - We're reading a set of slices of the volume
-		// - We're reading a set of scanlines of a slice
-		// - We're reading a set of voxels in a scanline
-		return ( size.x == dimensions.x && size.y == dimensions.y ) || ( size.x == dimensions.x && size.z == 1 ) || ( size.y == 1 && size.z == 1 );
-	}
-};
-
 class VMFOUNDATION_EXPORTS RawReaderIO
 {
 	std::string fileName;
@@ -94,5 +54,5 @@ private:
 	//}
 };
 
-}  // namespace ysl
+}  // namespace vm
 #endif
--- src/VMFoundation/rawreader.cpp
+++ src/VMFoundation/rawreader.cpp
@@ -4,107 +4,16 @@
 #include <VMFoundation/libraryloader.h>
 #include <VMFoundation/pluginloader.h>
 
-#include <cstring>	// memcpy
+#include <cstring>  // memcpy
 #include <cassert>
 #include <iostream>
 
 namespace vm
 {
-RawReader::RawReader( const std::string &fileName, const vm::Size3 &dimensions, size_t voxelSize ) :
-  fileName( fileName ), dimensions( dimensions ), voxelSize( voxelSize )  //,file(fileName,std::ios::binary),
-  ,
-  offset( 0 ),
-  ptr( nullptr )
-{
-	const auto rawBytes = dimensions.x * dimensions.y * dimensions.z * voxelSize;
-	auto repo = LibraryReposity::GetLibraryRepo();
-	assert( repo );
-	repo->AddLibrary( "ioplugin" );
-
-#ifdef _WIN32
-	io = PluginLoader::GetPluginLoader()->CreatePlugin<IFileMapping>( "windows" );
-#elif defined( __linux__ )
-	io = PluginLoader::GetPluginLoader()->CreatePlugin<IFileMapping>( "linux" );
-#endif
-	if ( io == nullptr )
-		throw std::runtime_error( "can not load ioplugin" );
-	io->Open( fileName, rawBytes, FileAccess::Read, MapAccess::ReadOnly );
-	ptr = io->FileMemPointer( 0, rawBytes );
-	if ( !ptr ) {
-		throw std::runtime_error( "map file failed RawReader::RawReader" );
-	}
-}
-
-RawReader::~RawReader()
-{
-	io->DestroyFileMemPointer( ptr );
-	io->Close();
-}
-// Read a region of volume data from the file into the buffer passed. It's assumed
-// the buffer passed has enough room. Returns the number voxels read
-size_t RawReader::readRegion( const vm::Size3 &start, const vm::Size3 &size, unsigned char *buffer )
-{
-	seekAmt = 0;
-	return readRegion__( start, size, buffer );
-}
-
-std::size_t RawReader::readRegion__( const vm::Size3 &start, const vm::Size3 &size, unsigned char *buffer )
-{
-	assert( size.x > 0 && size.y > 0 && size.z > 0 );
-	const uint64_t startRead = ( start.x + dimensions.x * ( start.y + dimensions.y * start.z ) ) * voxelSize;
-	if ( offset != startRead ) {
-		/*	seekAmt += startRead - offset;
-				std::cout << seekAmt<< std::endl;
-
-				seekAmt = startRead - offset;
-				if (!file.seekg(seekAmt, std::ios_base::cur))
-				{
-					throw std::runtime_error("ImportRAW: Error seeking file");
-				}
-
-				if (fseek(file, seekAmt, SEEK_CUR) != 0)
-				{
-					throw std::runtime_error("ImportRAW: Error seeking file");
-				}*/
-
-		offset = startRead;
-	}
-
-	// Figure out how to actually read the region since it may not be a full X/Y slice and
-	// we'll need to read the portions in X & Y and seek around to skip regions
-	size_t read = 0;
-	if ( convexRead( size ) )  // continuous read
-	{
-		//read = fread(buffer, voxelSize, size.x * size.y * size.z, file);
-
-		//file.read(reinterpret_cast<char*>(buffer), voxelSize* size.x * size.y * size.z);
-		//read = file.gcount()/voxelSize;
-
-		memcpy( reinterpret_cast<void *>( buffer ), ptr + offset, voxelSize * size.x * size.y * size.z );
-
-		read = size.x * size.y * size.z;  // voxel count
-
-		offset = startRead + read * voxelSize;
-	} else if ( size.x == dimensions.x ) {	// read by slice
-		for ( auto z = start.z; z < start.z + size.z; ++z ) {
-			const vm::Size3 startSlice( start.x, start.y, z );
-			const vm::Size3 sizeSlice( size.x, size.y, 1 );
-			read += readRegion__( startSlice, sizeSlice, buffer + read * voxelSize );
-		}
-	} else {
-		for ( auto z = start.z; z < start.z + size.z; ++z ) {
-			for ( auto y = start.y; y < start.y + size.y; ++y ) {
-				const vm::Size3 startLine( start.x, y, z );
-				const vm::Size3 sizeLine( size.x, 1, 1 );
-				read += readRegion__( startLine, sizeLine, buffer + read * voxelSize );
-			}
-		}
-	}
-	return read;
-}
-
 RawReaderIO::RawReaderIO( const std::string &fileName, const vm::Size3 &dimensions, size_t voxelSize ) :
-  fileName( fileName ), dimensions( dimensions ), voxelSize( voxelSize )  //,file(fileName,std::ios::binary),
+  fileName( fileName ),
+  dimensions( dimensions ),
+  voxelSize( voxelSize )  //,file(fileName,std::ios::binary),
   ,
   offset( 0 ),
   ptr( nullptr )
@@ -154,7 +63,7 @@ std::size_t RawReaderIO::readRegion__( const vm::Vec3i &start, const vm::Size3 &
 		read = size.x * size.y * size.z;  // voxel count
 
 		offset = startRead + read * voxelSize;
-	} else if ( size.x == dimensions.x ) {	// read by slice
+	} else if ( size.x == dimensions.x ) {  // read by slice
 		for ( auto z = start.z; z < start.z + size.z; ++z ) {
 			const vm::Vec3i startSlice( start.x, start.y, z );
 			const vm::Size3 sizeSlice( size.x, size.y, 1 );
@@ -171,4 +80,4 @@ std::size_t RawReaderIO::readRegion__( const vm::Vec3i &start, const vm::Size3 &
 	}
 	return read;
 }
-}  // namespace ysl
+}  // namespace vm
