618c46bbc322a67041ea2f0aae95dac5c57e995b
ysl <visysl@outlook.com>
2021-04-29 23:31:10 +0800

    delete comment

--- src/VMFoundation/cachepolicy.cpp
+++ src/VMFoundation/cachepolicy.cpp
@@ -5,30 +5,6 @@
 #include <map>
 #include <VMFoundation/logger.h>
 
-//#define PGSIZE 4096	 // bytes per page
-//#define PGSHIFT 12	 // bits of offset within a page
-//
-//#define PGROUNDUP( sz ) ( ( ( sz ) + PGSIZE - 1 ) & ~( PGSIZE - 1 ) )
-//#define PGROUNDDOWN( a ) ( ( ( a ) ) & ~( PGSIZE - 1 ) )
-//
-//#define PTE_V ( 1L << 0 )  // valid
-//#define PTE_R ( 1L << 1 )
-//#define PTE_W ( 1L << 2 )
-//#define PTE_X ( 1L << 3 )
-//#define PTE_U ( 1L << 4 )  // 1 -> user can access
-//
-//// shift a physical address to the right place for a PTE.
-//#define PA2PTE( pa ) ( ( ( (uint64)pa ) >> 12 ) << 10 )
-//
-//#define PTE2PA( pte ) ( ( ( pte ) >> 10 ) << 12 )
-//
-//#define PTE_FLAGS( pte ) ( (pte)&0x3FF )
-//
-//// extract the three 9-bit page table indices from a virtual address.
-//#define PXMASK 0x1FF  // 9 bits
-//#define PXSHIFT( level ) ( PGSHIFT + ( 9 * ( level ) ) )
-//#define PX( level, va ) ( ( ( ( uint64 )( va ) ) >> PXSHIFT( level ) ) & PXMASK )
-
 namespace vm
 {
 class ListBasedLRUCachePolicy__pImpl
